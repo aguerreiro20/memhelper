18673c553d4752de95348b92997ad529e138284b
aguerreiro20 <guerreiroa28@gmail.com>
2024-04-03 16:54:36 +0100

    flattened distribution

--- distribution/run.cmd
+++ distribution/run.cmd
@@ -1,2 +1,2 @@
 chcp 65001
-java -jar .\resources\app.jar
+java -jar app.jar
--- distribution/update.cmd
+++ distribution/update.cmd
@@ -1,1 +1,1 @@
-curl https://github.com/aguerreiro20/memhelper/blob/main/distribution/resources/app.jar -O .\resources\app.jar
+curl https://github.com/aguerreiro20/memhelper/blob/main/distribution/resources/app.jar -O app.jar
